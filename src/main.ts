7fbd163349f6b01b97c7714f0398598644efa8e3
Alvaro saiz <hello@alvarodesigns.com>
2024-11-18 22:43:45 +0100

    chore: :package: vercel / raiz

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();
